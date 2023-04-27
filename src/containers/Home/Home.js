823c05e317ec4f56ab1fff2709a134863b52ad20
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-04-27 21:40:17 +0530

    chore: project pickup

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -3,6 +3,7 @@ import "./Home.css";
 import Header from "../../components/Header/Header";
 import Footer from "../../components/Footer/Footer";
 import StartButton from "../../components/common/StartButton/StartButton";
+import AnimatedCover from "../../components/AnimatedCover/AnimatedCover";
 
 function Home() {
   return (
@@ -10,12 +11,11 @@ function Home() {
       <Header />
       <main className="home-main">
         <div className="home-back-cover"></div>
+        <AnimatedCover />
         <div className="home-cover">
           <div className="home-logo">
             <img className="cover-image" src={vapLight} alt="VAP Logo" />
-            <span className="cover-name">
-              vAP
-            </span>
+            <span className="cover-name">vAP</span>
           </div>
           <p className="cover-line">Visual Audio Processor</p>
           <StartButton>Start</StartButton>
