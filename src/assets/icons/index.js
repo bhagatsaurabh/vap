dde5f4f5ae7712aeb35c36819e953ddff7fbc248
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-04-30 19:38:09 +0530

    homepage

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -4,3 +4,4 @@ export { default as logo } from "./logo.png";
 export { default as flow } from "./flow.png";
 export { default as warning } from "./warning.png";
 export { default as spinner } from "./spinner.png";
+export { default as top } from "./top.png";
---
+++ src/components/common/ScrollToTop/ScrollToTop.jsx
@@ -0,0 +1,21 @@
+import Button from "../Button/Button";
+import styles from "./ScrollToTop.module.css";
+
+const ScrollToTop = ({ anchor, show }) => {
+  const classes = [styles["scroll-to-top"]];
+  if (show) {
+    classes.push(styles.show);
+  }
+
+  const handleClick = () => {
+    anchor()?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
+  };
+
+  return (
+    <div className={classes.join(" ")}>
+      <Button icon="top" accent="dark" onClick={handleClick} />
+    </div>
+  );
+};
+
+export default ScrollToTop;
