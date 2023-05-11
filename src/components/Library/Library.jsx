f4265636878ae2e313c1481a79a001ed72a675e2
Saurabh Bhagat <saurabhbhagat98die@gmail.com>
2023-05-11 22:37:56 +0530

    library

--- src/components/Library/Library.jsx
+++ src/components/Library/Library.jsx
@@ -1,31 +1,74 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import styles from "./Library.module.css";
+import Backdrop from "../common/Backdrop/Backdrop";
+import { useLocation, useNavigate, useNavigationType } from "react-router-dom";
 
 const Library = () => {
   const [open, setOpen] = useState(false);
   const [show, setShow] = useState(false);
+  const navigate = useNavigate();
+  const action = useNavigationType();
+  const location = useLocation();
 
-  const handleTransitionEnd = () => {
-    if (open) setShow(false);
+  useEffect(() => {
+    const path = location.pathname;
+    if (!open && history.state.idx === 0 && location.hash === "#library") {
+      navigate(path.substring(0, path.indexOf("#")), { replace: true });
+    }
+  }, []);
+  useEffect(() => {
+    if (open && action === "POP") {
+      setOpen(false);
+    }
+  }, [location]);
+  useEffect(() => {
+    if (show) {
+      navigate("#library", { state: { popUp: true }, replace: location.hash === "#library" });
+    }
+  }, [show]);
+
+  const handleOpen = () => {
+    if (!open) {
+      setOpen(true);
+      setShow(true);
+    } else {
+      handleDismiss();
+    }
+  };
+  const handleDismiss = () => {
+    if (show) {
+      navigate(-1);
+      setOpen(false);
+    }
+  };
+  const handleEnd = () => {
+    if (!open) {
+      setShow(false);
+    }
   };
 
   const classes = [styles.library];
   if (open) classes.push(styles.open);
 
   return (
-    <aside onTransitionEnd={handleTransitionEnd} className={classes.join(" ")}>
-      <button className={styles.control}>Library</button>
-      {show && (
-        <>
-          <div className={styles.header}>
-            <h1>Library</h1>
-            <input type="text" placeholder="Search" />
-          </div>
-          <div className={styles.content}></div>
-        </>
-      )}
-    </aside>
+    <>
+      <Backdrop show={show} onDismiss={handleDismiss} clear />
+      <aside onTransitionEnd={handleEnd} className={classes.join(" ")}>
+        <button onClick={handleOpen} className={styles.control}>
+          <p>Library</p>
+        </button>
+        {show && (
+          <>
+            <div className={styles.header}>
+              <h1>Library</h1>
+              <input type="text" placeholder="Search" />
+            </div>
+            <div className={styles.content}></div>
+          </>
+        )}
+      </aside>
+    </>
   );
 };
 
