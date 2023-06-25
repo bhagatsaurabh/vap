b0b75cb8028b4d2c14f0e522cbf9f604dd76fb30
saurabh-prosoft <saurabhbhagat98die@gmail.com>
2023-06-25 21:37:21 +0530

    refactor

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+
 import App from "@/App";
 import "@/index.css";
-import { Provider } from "react-redux";
 import store from "@/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
